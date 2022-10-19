f9ecd2b414b7bdb98261b67e9cd5a253e9bf6cb2
Me2023 <1287923048@qq.com>
2022-10-19 21:49:14 +0800

    first commit

---
+++ homework/homework1/homework1.py
@@ -0,0 +1,18 @@
+# Q1: A Plus Abs B
+def a_plus_abs_b(a, b):
+    """Return a+abs(b), but without calling abs.
+
+    >>> a_plus_abs_b(2, 3)
+    5
+    >>> a_plus_abs_b(2, -3)
+    5
+    >>> a_plus_abs_b(-1, 4)
+    3
+    >>> a_plus_abs_b(-1, -4)
+    3
+    """
+    if b < 0:
+        f = a - b
+    else:
+        f = a + b
+    return f(a, b)
